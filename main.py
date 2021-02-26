983e086f8c9216ab870d5bf196196872b5e682f6
timomops <timomop66@gmail.com>
2021-02-26 18:13:34 +0100

    ajout (un)mute / purge / command help

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 #import discord
 import os
+import time
 from discord.ext import commands
+from discord.ext.commands import Bot, has_permissions, CheckFailure
 from discord.utils import get
 import discord
 from decouple import config
 
 token= config('key')
 
-client = commands.Bot(command_prefix = '+', help_command=None)
+client = commands.Bot(command_prefix = '+')
+client.remove_command("help")
 
 privatemessage = """Candidature - Clarity
 1)Qui es-tu ?
@@ -24,6 +27,29 @@ alreadyapply = "Un channel existe déja pour ton apply"
 async def on_ready():
   print(f'{client.user} has Awoken!')
 
+@client.group(invoke_without_command=True)
+async def help(ctx):
+  em = discord.Embed(title = "Help", description = "Use +help <command>")
+
+  em.add_field(name = "Apply", value="Candidature")
+  em.add_field(name = "Admin",value="clean,purge")
+  em.add_field(name = "Other", value="ping")
+
+  await ctx.send(embed =em)
+
+@help.command()
+async def purge(ctx):
+  em = discord.Embed(title = "Purge", description= "Supprimer x messages dans le channel",color = ctx.author.color)
+  em.add_field(name = "**Syntax**", value = "+purge <nombre>")
+  await ctx.send(embed = em)
+
+@help.command()
+async def clean(ctx):
+  em = discord.Embed(title = "Clean", description= "Permet de supprimer le channel d'un apply",color = ctx.author.color)
+  em.add_field(name = "**Syntax**", value = "+clean <nom>")
+  await ctx.send(embed = em)
+
+#Apply clarity
 @client.command()
 async def Candidature(ctx):
   if ctx.channel.name == 'apply':
@@ -72,20 +98,65 @@ async def Candidature(ctx):
         await ctx.author.send(privatemessage)    
     await ctx.message.delete()
 
+#Ping bot
+@client.command()
+async def ping(ctx):
+     await ctx.send(f'Pong! In {round(client.latency * 1000)}ms')
+     await ctx.message.delete()
+
+#Purge x message
+@client.command(pass_context=True)
+async def purge(ctx, limit: int):
+  guild= ctx.message.guild
+  checkRole = discord.utils.get(ctx.guild.roles, name="Sainte trinité")
+  if checkRole in ctx.author.roles:
+    await ctx.message.delete()
+    await ctx.channel.purge(limit=limit)
+  else:
+    msg = "Non, je crois pas : {}".format(ctx.message.author.mention)
+    await ctx.send(msg)
+
+#Clean channel apply
 @client.command()
 async def clean(ctx,username):
   guild = ctx.message.guild
-#Delete channel
+  #Delete channel
   channelName = 'candidature-'+str(username)
   channel =  discord.utils.get(ctx.guild.channels, name=channelName)
   await channel.delete()
-##TODO MAKE DELETE ROLE WORKING##
+  ##TODO MAKE DELETE ROLE WORKING##
 
-#Delete role
+  #Delete role
   #roleName='Candidature '+str(username)
   #print(f'-----------------------------------------')
   #role = discord.utils.get(ctx.message.roles, name=roleName)
   #print(f'*****************************************')
   #await client.remove_roles(role)
+
+#Mute all except Sainte trinité
+@client.command()
+async def mute(ctx):
+  vc = ctx.author.voice.channel
+  for member in vc.members:
+    tmp = 0
+    #List
+    for role in member.roles:
+      if ('Sainte trinité' in str(role)):
+        tmp = 1
+        print(f'{member} à sainte trinité')
+        
+    if tmp != 0:
+      await member.edit(mute=False)
+    else:
+      await member.edit(mute=True)    
+
+#Unmute all
+@client.command()
+async def unmute(ctx):
+  vc = ctx.author.voice.channel
+  guild= ctx.message.guild
+  for member in vc.members:
+    await member.edit(mute=False)
+
 
 client.run(token)
